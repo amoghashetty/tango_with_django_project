e261ec33efce59e1c17c80e9afa38c3321e70ac4
Amogha Shetty <2622918s@student.gla.ac.uk>
2023-02-10 13:30:27 +0000

    Chapter 10 exercises completed

--- populate_rango.py
+++ populate_rango.py
@@ -6,12 +6,6 @@ django.setup()
 from rango.models import Category , Page 
 
 def populate():
-    # First, we will create lists of dictionaries containing the pages
-    # we want to add into each category.
-    # Then we will create a dictionary of dictionaries for our categories. 
-    # This might seem a little bit confusing, but it allows us to iterate 
-    # through each data structure, and add the data to our models.
-
     python_pages = [
         {'title': 'Official Python Tutorial',
          'url' : 'http://docs.python.org/3/tutorial/','views': 111,},
@@ -45,17 +39,11 @@ def populate():
             'Other Frameworks': {'pages': other_pages, 'views': 32, 'likes': 16}
     }
 
-    # If you want to add more categories or pages,
-    # add them to the dictionaries above.
-
-    # The code below goes through the cats dictionary, then adds each category, 
-    # and then adds all the associated pages for that category.
     for cat, cat_data in cats.items():
         c = add_cat(cat, views=cat_data['views'], likes=cat_data['likes'])
         for p in cat_data['pages']:
             add_page(c, p['title'], p['url'], views=p['views'])
     
-    #Print out the categories we have added.
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
             print(f'- {c}: {p}')
@@ -74,7 +62,6 @@ def add_cat(name, views=0, likes=0):
     c.save()
     return c
 
-# Start execution here!
 if __name__ == '__main__':
     print('Starting Rango population script...')
     populate()
